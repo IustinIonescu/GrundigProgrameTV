b521619a4416b070c41191c977bb73448efa79f5
BuildTools <unconfigured@null.spigotmc.org>
2023-07-08 22:20:35 +0300

    Add project files.

---
+++ Programe TV/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Programe_TV
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.sortButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadButton.Location = new System.Drawing.Point(12, 12);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(100, 40);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click_1);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToOrderColumns = true;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 81);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView.RowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.Size = new System.Drawing.Size(776, 357);
+            this.dataGridView.TabIndex = 1;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            this.dataGridView.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellValueChanged);
+            this.dataGridView.DragDrop += new System.Windows.Forms.DragEventHandler(this.DataGridView_DragDrop);
+            this.dataGridView.DragOver += new System.Windows.Forms.DragEventHandler(this.DataGridView_DragOver);
+            this.dataGridView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DataGridView_MouseDown);
+            this.dataGridView.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DataGridView_MouseMove);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(122, 12);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(100, 40);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click_1);
+            // 
+            // sortButton
+            // 
+            this.sortButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sortButton.Location = new System.Drawing.Point(232, 12);
+            this.sortButton.Name = "sortButton";
+            this.sortButton.Size = new System.Drawing.Size(100, 40);
+            this.sortButton.TabIndex = 3;
+            this.sortButton.Text = "Sort";
+            this.sortButton.UseVisualStyleBackColor = true;
+            this.sortButton.Click += new System.EventHandler(this.sortButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(354, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 38);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.sortButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.loadButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "MainForm";
+            this.Text = "Programe TV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button sortButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Programe TV/Form1.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Programe_TV
+{
+    public partial class MainForm : Form
+    {
+        private DataTable dataTable;
+        private XmlDocument document;
+        private int draggedRowIndex = -1;
+        private DataView filteredView; // Added DataView for filtering
+        public MainForm()
+        {
+            InitializeComponent();
+
+
+
+            dataTable = new DataTable();
+            dataTable.Columns.Add("Nume Program");
+            dataTable.Columns.Add("Pozitie", typeof(int));
+            //dataGridView.DataSource = dataTable;
+
+            filteredView = new DataView(dataTable); // Initialize the DataView
+            filteredView.Sort = "Pozitie ASC"; // Set initial sort by "Num" column
+
+            dataGridView.DataSource = filteredView;
+            dataTable = filteredView.ToTable();
+
+            // Enable drag and drop in the DataGridView
+            dataGridView.AllowDrop = true;
+
+
+            // Hide the row selector column
+            dataGridView.RowHeadersVisible = false;
+
+            this.Resize += MainForm_Resize;
+
+            this.DragEnter += MainForm_DragEnter;
+            this.DragDrop += MainForm_DragDrop;
+            dataGridView.DragEnter += MainForm_DragEnter;
+            dataGridView.DragDrop += MainForm_DragDrop;
+            dataGridView.CellBeginEdit += dataGridView_CellBeginEdit;
+
+        }
+
+        private void dataGridView_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            // Check if the first column is being edited
+            if (e.ColumnIndex == 0)
+            {
+                // Cancel the edit operation for the first column
+                e.Cancel = true;
+            }
+        }
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files != null && files.Length > 0)
+            {
+                string filePath = files[0];
+
+                // Load the XML file and populate the DataGridView
+                loadXml(filePath);
+            }
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            // Adjust the column widths based on percentage
+            int serviceNameColumnWidth = (int)(dataGridView.Width * 0.76);
+            int numColumnWidth = (int)(dataGridView.Width * 0.2);
+
+            dataGridView.Columns["Nume Program"].Width = serviceNameColumnWidth;
+            dataGridView.Columns["Pozitie"].Width = numColumnWidth;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int serviceNameColumnWidth = (int)(dataGridView.Width * 0.76);
+            int numColumnWidth = (int)(dataGridView.Width * 0.2);
+
+            dataGridView.Columns["Nume Program"].Width = serviceNameColumnWidth;
+            dataGridView.Columns["Pozitie"].Width = numColumnWidth;
+
+
+        }
+
+
+        private void saveButton_Click_1(object sender, EventArgs e)
+        {
+            if (document != null)
+            {
+
+
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    // Get the service name and new num value from the DataGridView
+                    DataRow dataRow = dataTable.Rows[i];
+                    string serviceName = dataRow["Nume Program"].ToString();
+                    int newNumValue = Convert.ToInt32(dataRow["Pozitie"]);
+
+                    // Search for the XML element based on the service name attribute value
+                    XmlNodeList serviceNodes = document.SelectNodes($"//service[@name='{serviceName}']");
+                    if (serviceNodes.Count > 0)
+                    {
+                        XmlNode serviceNode = serviceNodes[0];
+
+                        // Update the num attribute value
+                        XmlAttribute numAttribute = serviceNode.Attributes["num"];
+                        if (numAttribute != null)
+                        {
+                            numAttribute.Value = newNumValue.ToString();
+                        }
+                        else
+                        {
+                            // If the num attribute doesn't exist, create it
+                            numAttribute = document.CreateAttribute("num");
+                            numAttribute.Value = newNumValue.ToString();
+                            serviceNode.Attributes.Append(numAttribute);
+                        }
+                    }
+                }
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "XML Files|*.xml";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    document.Save(saveFileDialog.FileName);
+                    MessageBox.Show("Modified XML saved successfully.");
+                }
+            }
+        }
+
+        private void loadButton_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "XML Files|*.xml";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+
+                loadXml(fileName);
+            }
+        }
+
+        private void loadXml(string fileName)
+        {
+            document = new XmlDocument();
+
+            document.Load(fileName);
+
+            XmlNodeList serviceNodes = document.SelectNodes("//service[./@typ='1']");
+
+            dataTable.Rows.Clear();
+
+            foreach (XmlNode serviceNode in serviceNodes)
+            {
+                XmlElement serviceElement = (XmlElement)serviceNode;
+                string serviceName = serviceElement.GetAttribute("name");
+                int num = int.Parse(serviceElement.GetAttribute("num"));
+
+                dataTable.Rows.Add(serviceName, num);
+            }
+
+
+            filteredView = new DataView(dataTable); // Initialize the DataView
+            filteredView.Sort = "Pozitie ASC";
+
+            dataGridView.DataSource = filteredView;
+
+
+            dataTable = filteredView.ToTable();
+
+            if (dataGridView.Rows.Count > 0)
+            {
+                dataGridView.Rows[0].Selected = true;
+            }
+        }
+
+        private void DataGridView_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                draggedRowIndex = dataGridView.HitTest(e.X, e.Y).RowIndex;
+                //label1.Text = draggedRowIndex.ToString();
+                if (draggedRowIndex >= 0)
+                {
+                    dataGridView.DoDragDrop(dataGridView.Rows[draggedRowIndex], DragDropEffects.Move);
+                }
+            }
+        }
+
+        private void DataGridView_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left && draggedRowIndex >= 0)
+            {
+                dataGridView.DoDragDrop(dataGridView.Rows[draggedRowIndex], DragDropEffects.Move);
+            }
+        }
+
+        private void DataGridView_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void DataGridView_DragDrop(object sender, DragEventArgs e)
+        {
+            //label1.Text = draggedRowIndex.ToString();
+            if (draggedRowIndex >= 0)
+            {
+                Point clientPoint = dataGridView.PointToClient(new Point(e.X, e.Y));
+                int targetRowIndex = dataGridView.HitTest(clientPoint.X, clientPoint.Y).RowIndex;
+                //label2.Text = targetRowIndex.ToString();
+                if (targetRowIndex >= 0 && targetRowIndex != draggedRowIndex)
+                {
+                    DataRow draggedRow = dataTable.Rows[draggedRowIndex];
+                    DataRow newRow = dataTable.NewRow();
+                    newRow["Nume Program"] = draggedRow["Nume Program"];
+                    newRow["Pozitie"] = draggedRow["Pozitie"];
+
+                    //          DataRow draggedRow = ((DataRowView)e.Data.GetData(typeof(DataRowView))).Row;
+
+
+                    dataTable.Rows.RemoveAt(draggedRowIndex);
+                    dataTable.Rows.InsertAt(newRow, targetRowIndex);
+
+                    
+                    // Update the "Num" column in the DataTable
+                    for (int i = 0; i < dataTable.Rows.Count; i++)
+                    {
+                        dataTable.Rows[i]["Pozitie"] = (i + 1).ToString();
+                    }
+
+                    filteredView = new DataView(dataTable); // Initialize the DataView
+                    dataGridView.DataSource = filteredView;
+                    dataGridView.Refresh();
+
+                }
+            }
+            dataGridView.ClearSelection();
+
+            draggedRowIndex = -1;
+        }
+
+
+        private void dataGridView_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 1 && e.RowIndex >= 0) // Check if the "Num" column is edited
+            {
+                string newValue = dataGridView.Rows[e.RowIndex].Cells[e.ColumnIndex].Value?.ToString();
+                int currentRowPosition = e.RowIndex + 1;
+
+                // Check if the new value is numeric
+                if (int.TryParse(newValue, out int newRowPosition))
+                {
+
+                    if (newRowPosition >= 1 && newRowPosition <= dataGridView.RowCount)
+                    {
+                        if (newRowPosition != currentRowPosition)
+                        {
+                            DataRow draggedRow = dataTable.Rows[e.RowIndex];
+
+                            DataRow newRow = dataTable.NewRow();
+                            newRow["Nume Program"] = draggedRow["Nume Program"];
+                            newRow["Pozitie"] = draggedRow["Pozitie"];
+
+                            dataTable.Rows.Remove(draggedRow);
+                            dataTable.Rows.InsertAt(newRow, newRowPosition - 1);
+
+
+                            // Update the "Num" column in the DataTable
+                            for (int i = 0; i < dataTable.Rows.Count; i++)
+                            {
+                                dataTable.Rows[i]["Pozitie"] = (i + 1).ToString();
+                            }
+
+                            filteredView = new DataView(dataTable); // Initialize the DataView
+                            dataGridView.DataSource = filteredView;
+                            dataGridView.Refresh();
+                        }
+                    }
+                    else
+                    {
+                        // Reset the value if it's out of range
+                        dataGridView.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = currentRowPosition;
+                    }
+                }
+                else
+                {
+                    // Reset the value if it's not numeric
+                    dataGridView.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = currentRowPosition;
+                }
+            }
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void sortButton_Click(object sender, EventArgs e)
+        {
+            filteredView = new DataView(dataTable); // Initialize the DataView
+            filteredView.Sort = "Nume Program ASC";
+
+            dataTable = filteredView.ToTable();
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                dataTable.Rows[i]["Pozitie"] = (i + 1).ToString();
+            }
+
+            dataGridView.DataSource = new DataView(dataTable);
+            dataGridView.Refresh();
+
+            /*
+                for (int i = 0; i < dataGridView.Rows.Count; i++)
+                {
+                    if (dataGridView.Rows[i].Cells[0].Value == null)
+                        continue;
+                    string numeProgram = dataGridView.Rows[i].Cells[0].Value.ToString();
+
+                    for (int j=0; j< dataTable.Rows.Count; j++)
+                    {
+                        if (dataTable.Rows[j]["Nume Program"] == numeProgram)
+                        {
+                            dataTable.Rows[j]["Pozitie"] = i+1;
+                            break;
+                        }
+                    }
+                }
+            */
+
+
+
+            /*
+            // Sort the rows in the DataGridView by the "Service Name" column alphabetically
+            dataGridView.Sort(dataGridView.Columns["Nume Program"], ListSortDirection.Ascending);
+
+            // Update the "Num" values starting from 1 for the sorted rows
+            for (int i = 0; i < dataGridView.Rows.Count; i++)
+            {
+                dataGridView.Rows[i].Cells["Pozitie"].Value = (i + 1).ToString();
+            }
+            dataGridView.Refresh();
+*/
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView.ClearSelection();
+            string text = textBox1.Text;
+            int rowIndex = -1;
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                string programName = dataTable.Rows[i]["Nume Program"].ToString();
+                if (programName.ToLower().StartsWith(text.ToLower()))
+                {
+                    rowIndex = i;
+                    break;
+                }
+            }
+            if(rowIndex == -1)
+            {
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    string programName = dataTable.Rows[i]["Nume Program"].ToString();
+                    if (programName.ToLower().Contains(text.ToLower()))
+                    {
+                        rowIndex = i;
+                        break;
+                    }
+                }
+            }
+            if (rowIndex != -1 && dataGridView.Rows.Count > 0)
+            {
+                dataGridView.Rows[rowIndex].Selected = true;
+                dataGridView.FirstDisplayedScrollingRowIndex = rowIndex;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+    }
+}
